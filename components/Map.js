f797f0e92b382b53d66af206e8c4db81a09e7026
chenkv <boykevinchen@gmail.com>
2021-07-21 16:01:53 -0600

    Map code reorganization

---
+++ components/Map.js
@@ -0,0 +1,30 @@
+import { useEffect, useState } from "react";
+const mapboxgl = require("mapbox-gl/dist/mapbox-gl.js");
+
+function Map() {
+    const [pageIsMounted, setPageIsMounted] = useState(false);
+
+    mapboxgl.accessToken = 'pk.eyJ1IjoicGVuZ3Vpb29vIiwiYSI6ImNrcmNmeGc1eTUycTMydm10dGFqZmlyaTkifQ.RYQDCD2tHifk0r-ky8DEBw';
+
+    useEffect(() => {
+        setPageIsMounted(true);
+
+        let map = new mapboxgl.Map({
+            container: "map",
+            style: "mapbox://styles/mapbox/streets-v11",
+            center: [-77.02, 38.887],
+            zoom: 12.5,
+            pitch: 45,
+            maxBounds: [
+                [-77.875588, 38.50705],
+                [-76.15381, 39.548764],
+            ],
+        });
+    }, []);
+
+    return (
+        <div id="map" style={{ height: 500, width: 1000 }} />
+    );
+}
+
+export default Map;
--- pages/index.js
+++ pages/index.js
@@ -1,41 +1,18 @@
-import { useEffect, useState } from "react";
 import Head from 'next/head';
-const mapboxgl = require("mapbox-gl/dist/mapbox-gl.js");
 
 import Layout from '../components/Layout';
+import Map from '../components/Map';
 
-function Index() {
-    const [pageIsMounted, setPageIsMounted] = useState(false);
-
-    mapboxgl.accessToken = 'pk.eyJ1IjoicGVuZ3Vpb29vIiwiYSI6ImNrcmNmeGc1eTUycTMydm10dGFqZmlyaTkifQ.RYQDCD2tHifk0r-ky8DEBw';
-
-    useEffect(() => {
-        setPageIsMounted(true);
-
-        let map = new mapboxgl.Map({
-            container: "map",
-            style: "mapbox://styles/mapbox/streets-v11",
-            center: [-77.02, 38.887],
-            zoom: 12.5,
-            pitch: 45,
-            maxBounds: [
-                [-77.875588, 38.50705],
-                [-76.15381, 39.548764],
-            ],
-        });
-    }, []);
-
-    return (
-        <Layout>
-            <Head>  
-                <link href='https://api.mapbox.com/mapbox-gl-js/v2.3.1/mapbox-gl.css' rel='stylesheet' />
-            </Head>
-            <h1>test</h1>
-            <main>
-                <div id="map" style={{ height: 500, width: 1000 }} />
-            </main>
-        </Layout>
-    );
-}
+const Index = () => (
+    <Layout>
+        <Head>  
+            <link href='https://api.mapbox.com/mapbox-gl-js/v2.3.1/mapbox-gl.css' rel='stylesheet' />
+        </Head>
+        <h1>test</h1>
+        <main>
+            <Map/>
+        </main>
+    </Layout>
+)
 
 export default Index;
