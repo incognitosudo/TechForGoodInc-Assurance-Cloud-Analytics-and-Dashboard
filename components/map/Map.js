7ac4c8c3ba080c127566f233c452241a478610a8
chenkv <boykevinchen@gmail.com>
2021-07-29 14:29:28 -0600

    Basic pin points

--- components/map/Map.js
+++ components/map/Map.js
@@ -2,11 +2,18 @@ import { useEffect, useState } from "react";
 import useSWR from "swr";
 
 import fetcher from "../../utilities/fetcher";
+import { initializeMap } from "./initializeMap";
+import { addDataLayer } from "./addDataLayer";
 const mapboxgl = require("mapbox-gl/dist/mapbox-gl.js");
 
 function Map() {
     const [pageIsMounted, setPageIsMounted] = useState(false);
+    const [Map, setMap] = useState();
     const { data, error } = useSWR("/api/shootings", fetcher);
+
+    if (error) {
+        console.error(error);
+    }
 
     mapboxgl.accessToken = 'pk.eyJ1IjoicGVuZ3Vpb29vIiwiYSI6ImNrcmNmeGc1eTUycTMydm10dGFqZmlyaTkifQ.RYQDCD2tHifk0r-ky8DEBw';
 
@@ -19,21 +26,19 @@ function Map() {
             center: [-77.02, 38.887],
             zoom: 12.5,
             pitch: 45,
-            // maxBounds: [
-            //     [-77.875588, 38.50705],
-            //     [-76.15381, 39.548764],
-            // ],
         });
 
-        map.addControl(
-            new mapboxgl.GeolocateControl({
-                positionOptions: {
-                    enableHighAccuracy: true,
-                },
-                trackUserLocation: true,
-            })
-        );
+        initializeMap(mapboxgl, map);
+        setMap(map);
     }, []);
+
+    useEffect(() => {
+        if (pageIsMounted && data) {
+            Map.on("load", function () {
+                addDataLayer(Map, data);
+            });
+        }
+    }, [pageIsMounted, setMap, data, Map]);
 
     return (
         <div id="map" style={{ height: 500, width: 1000 }} />
