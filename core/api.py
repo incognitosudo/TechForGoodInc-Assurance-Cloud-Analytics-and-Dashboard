da7a0ee9cdbee2ad391fcfe54f75bab01e01aaee
Suhan Park <44104676+suhanpark@users.noreply.github.com>
2021-08-05 03:53:45 -0400

    auth

--- core/api.py
+++ core/api.py
@@ -2,16 +2,18 @@ from datetime import datetime, timedelta
 from fastapi import FastAPI, Path, Depends, HTTPException, status
 from typing import Optional
 from pydantic import BaseModel
-from connect import crsr, cnxn
+from database import Base
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from jose import JWTError, jwt
 from passlib.context import CryptContext
 import secrets
+from crud import get_user, authenticate_user
 
 
 SECRET_KEY = secrets.token_hex(20)
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
 app = FastAPI()
 
@@ -53,36 +55,11 @@ def sign_up(user: User):
 
 @app.get("/get-all-incidents")
 def get_all_incidents():
-    incidents = []
-    crsr.execute("SELECT * FROM INCIDENTS")
-    each_incident = {}
-    for row in crsr.fetchall():
-        each_incident = {
-            "incident_id": row[0],
-            "date": row[1],
-            "state": row[2],
-            "city": row[3],
-            "address": row[4],
-            "killed": row[5],
-            "injuried": row[6],
-        }
-        incidents.append(each_incident)
-    return {"Data": incidents}
+    pass
 
 @app.get("/get-incident/{incident_id}")
 def get_incident_by_id(incident_id: str):
-    crsr.execute(f"SELECT * FROM INCIDENTS where IncidentID={incident_id}") 
-    row = crsr.fetchall()[0];
-    incident = {
-        "incident_id": row[0],
-        "date": row[1],
-        "state": row[2],
-        "city": row[3],
-        "address": row[4],
-        "killed": row[5],
-        "injuried": row[6],
-    }
-    return {"Data": incident}
+    pass
 
 
 def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
@@ -94,3 +71,50 @@ def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
     to_encode.update({"exp": expire})
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
+
+
+async def get_current_user(token: str = Depends(oauth2_scheme)):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get("sub")
+        if username is None:
+            raise credentials_exception
+        token_data = TokenData(username=username)
+    except JWTError:
+        raise credentials_exception
+    user = get_user(Base, username=token_data.username)
+    if user is None:
+        raise credentials_exception
+    return user
+
+
+async def get_current_active_user(current_user: User = Depends(get_current_user)):
+    if current_user.disabled:
+        raise HTTPException(status_code=400, detail="Inactive user")
+    return current_user
+
+
+@app.post("/token", response_model=Token)
+async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends()):
+    user = authenticate_user(Base, form_data.username, form_data.password)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = create_access_token(
+        data={"sub": user.username}, expires_delta=access_token_expires
+    )
+    return {"access_token": access_token, "token_type": "bearer"}
+
+
+@app.get("/users/me/", response_model=User)
+async def read_users_me(current_user: User = Depends(get_current_active_user)):
+    return current_user
